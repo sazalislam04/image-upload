cb001e2dc75c6da3aad35c5db977806ee21de910
sazalislam04 <mdsazalislam04@gmail.com>
2022-12-04 23:29:06 +0600

    added bootstrap

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
+const defaultBtn = document.getElementById("default-btn");
+const imagePreview = document.getElementById("image");
+const imageContainer = document.querySelector(".image-container");
+const handleUploadImage = () => {
+  defaultBtn.click();
+};
 
+defaultBtn.addEventListener("change", () => {
+  const file = document.querySelector("input[type=file]").files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const result = reader.result;
+      imagePreview.src = result;
+    };
+    reader.readAsDataURL(file);
+    imageContainer.style.display = "block";
+  }
+});
